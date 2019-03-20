d8d74212f81882ca336b6814fa2f1b50858b023d
Michael Stanley <michaelstanley@Justins-MacBook-Air.local>
2019-03-19 17:33:20 -0700

    modified:   calc.js 	new file:   calcStyle.css 	modified:   calculator.html

--- calc.js
+++ calc.js
@@ -1,37 +1,77 @@
-//Added to git? 
+//Added to git?
 function displayResults(event){
-  var pp = document.getElementById('perPersonCost').value;
+  var parent = document.getElementById("results");
+  while(parent.firstChild)
+  {
+    parent.removeChild(parent.firstChild);
+  }
+
+
+  var gc = document.getElementById('totalGC').value;
   var count = document.getElementById('count').value;
-  var total = pp * count;
   var addition = document.getElementById('results');
+  var totalPrice = document.getElementById('totalCost').value;
 
   addition.appendChild(document.createElement("table"));
+  var percentHeader = document.createElement("th");
+  percentHeader.textContent = "Discount %";
+  addition.appendChild(percentHeader);
+
   for(var i = 5; i <= 100; i += 10)
   {
     var newHead = document.createElement("th");
     newHead.textContent = " " + i + "% ";
-    newHead.style.border = "thin solid"
-    //newhead.style.width = "50";
     addition.appendChild(newHead);
     if(i == 5)
     {
       i = 0;
     }
   }
-  var newRow = document.createElement("tr");
-  for(var j = 5; j <= 100; j+= 10)
-  {
-    var newCell = document.createElement("td");
-    var n = (j / 100) * (pp * count);
-    newCell.textContent = "  " + n.toFixed(2) + "  ";
-    newCell.style.border = "thin solid";
-    newRow.appendChild(newCell);
-    if(j == 5)
+
+event.preventDefault();
+  function calculateDiscount(){
+    var newRow = document.createElement("tr");
+    var dVal = document.createElement("td");
+    var perPerson = totalPrice / gc;
+    dVal.textContent = "Discount Value(" + perPerson + " * Item Count)";
+    newRow.appendChild(dVal);
+    for(var j = 5; j <= 100; j+= 10)
     {
-      j = 0;
+      var newCell = document.createElement("td");
+      var n = (j / 100) * ((totalPrice / gc) * count);
+      newCell.textContent = "  " + n.toFixed(2) + "  ";
+      newRow.appendChild(newCell);
+      if(j == 5)
+      {
+        j = 0;
+      }
     }
+    addition.appendChild(newRow);
+    var newPrice = document.createElement("tr");
+    var npHeader = document.createElement("td");
+    npHeader.textContent = "New Cost";
+    newPrice.appendChild(npHeader);
+    for(var j = 5; j <= 100; j+= 10)
+    {
+      var newCell = document.createElement("td");
+      var n = totalPrice - (j / 100) * ((totalPrice / gc) * count);
+      newCell.textContent = "  " + n.toFixed(2) + "  ";
+      newPrice.appendChild(newCell);
+      if(j == 5)
+      {
+        j = 0;
+      }
+    }
+    addition.appendChild(newPrice);
+
   }
-  addition.appendChild(newRow);
+
+
+
+    calculateDiscount()
+
+
+
   event.preventDefault();
 }
 
